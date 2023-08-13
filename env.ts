b7417115a46ff132e4a6d206be79ad5b54b7c77e
bdwd <cg.bdwd@gmail.com>
2023-08-13 10:29:59 -0400

    (add) set up database

--- env.ts
+++ env.ts
@@ -21,6 +21,15 @@ export default Env.rules({
 	APP_NAME: Env.schema.string(),
 	CACHE_VIEWS: Env.schema.boolean(),
 	SESSION_DRIVER: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
+	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+
+	PG_HOST: Env.schema.string({ format: 'host' }),
+	PG_PORT: Env.schema.number(),
+	PG_USER: Env.schema.string(),
+	PG_PASSWORD: Env.schema.string.optional(),
+	PG_DB_NAME: Env.schema.string(),
+
+	ADMIN_USER: Env.schema.string(),
+	ADMIN_PASSWORD: Env.schema.string(),
 })
